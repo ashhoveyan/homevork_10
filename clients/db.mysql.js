a45f19ee3abad8f30461ae56d99e078dbbfde61d
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-09 13:49:21 +0400

    may be finish

--- clients/db.mysql.js
+++ clients/db.mysql.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'myfirstdb'
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 const connection = mysql.createConnection(dbConfig);
